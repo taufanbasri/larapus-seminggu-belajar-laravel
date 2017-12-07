eb24733dbcd12c09c4747eeab6d4dc94576ef1df
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2017-12-07 05:08:58 +0000

    Book CRUD

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Book;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Html\Builder;
+use DataTables;
+use Session;
+use File;
 
 class BookController extends Controller
 {
@@ -12,9 +16,31 @@ class BookController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request, Builder $htmlBuilder)
     {
-        //
+      if ($request->ajax()) {
+        $books = Book::with('author');
+
+        return Datatables::of($books)
+          ->addColumn('action', function($book){
+            return view('datatable._action', [
+              'model' => $book,
+              'form_url' => route('books.destroy', $book->id),
+              'edit_url' => route('books.edit', $book->id),
+              'confirm_message' => 'Yakin akan menghapus ' . $book->title . '?'
+            ]);
+          })->toJson();
+      }
+
+      $html = $htmlBuilder
+        ->columns([
+          ['data' => 'title', 'name' => 'title', 'title' => 'Judul'],
+          ['data' => 'amount', 'name' => 'amount', 'title' => 'Jumlah'],
+          ['data' => 'author.name', 'name' => 'author.name', 'title' => 'Penulis'],
+          ['data' => 'action', 'name' => 'action', 'title' => '', 'orderable' => false, 'searchable' => false]
+        ]);
+
+      return view('books.index')->with(compact('html'));
     }
 
     /**
@@ -24,7 +50,7 @@ class BookController extends Controller
      */
     public function create()
     {
-        //
+        return view('books.create');
     }
 
     /**
@@ -35,7 +61,31 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $this->validate($request, [
+        'title' => 'required|unique:books,title',
+        'author_id' => 'required|exists:authors,id',
+        'amount' => 'required|numeric',
+        'cover' => 'image|max:2048'
+      ]);
+
+      $book = Book::create($request->except('cover'));
+
+      if ($request->hasFile('cover')) {
+        $uploaded_cover = $request->file('cover');
+        $extension = $uploaded_cover->getClientOriginalExtension();
+        $filename = md5(time()) . '.' . $extension;
+        $destinationPath = public_path() . DIRECTORY_SEPARATOR . 'img';
+        $uploaded_cover->move($destinationPath, $filename);
+        $book->cover = $filename;
+        $book->save();
+      }
+
+      Session::flash("flash_notification", [
+      "level"=>"success",
+      "message"=>"Berhasil menyimpan $book->title"
+      ]);
+
+      return redirect()->route('books.index');
     }
 
     /**
@@ -57,7 +107,7 @@ class BookController extends Controller
      */
     public function edit(Book $book)
     {
-        //
+        return view('books.edit')->with(compact('book'));
     }
 
     /**
@@ -69,7 +119,45 @@ class BookController extends Controller
      */
     public function update(Request $request, Book $book)
     {
-        //
+      $this->validate($request, [
+        'title' => 'required|unique:books,title,' . $book->id,
+        'author_id' => 'required|exists:authors,id',
+        'amount' => 'required|numeric',
+        'cover' => 'image|max:2048'
+      ]);
+
+      $book->update($request->all());
+
+      if ($request->hasFile('cover')) {
+        $filename = null;
+        $uploaded_cover = $request->file('cover');
+        $extension = $uploaded_cover->getClientOriginalExtension();
+        $filename = md5(time()) . '.' . $extension;
+        $destinationPath = public_path() . DIRECTORY_SEPARATOR . 'img';
+        $uploaded_cover->move($destinationPath, $filename);
+
+        if ($book->cover) {
+          $old_cover = $book->cover;
+          $filepath = public_path() . DIRECTORY_SEPARATOR . 'img'
+          . DIRECTORY_SEPARATOR . $book->cover;
+
+          try {
+            File::delete($filepath);
+          } catch (FileNotFoundException $e) {
+            // File sudah dihapus/tidak ada
+          }
+        }
+
+        $book->cover = $filename;
+        $book->save();
+      }
+
+      Session::flash("flash_notification", [
+        "level"=>"success",
+        "message"=>"Berhasil menyimpan $book->title"
+      ]);
+
+      return redirect()->route('books.index');
     }
 
     /**
@@ -80,6 +168,25 @@ class BookController extends Controller
      */
     public function destroy(Book $book)
     {
-        //
+      if ($book->cover) {
+        $old_cover = $book->cover;
+        $filepath = public_path() . DIRECTORY_SEPARATOR . 'img'
+        . DIRECTORY_SEPARATOR . $book->cover;
+
+        try {
+          File::delete($filepath);
+        } catch (FileNotFoundException $e) {
+          // File sudah dihapus/tidak ada
+        }
+      }
+
+      $book->delete();
+
+      Session::flash("flash_notification", [
+        "level"=>"success",
+        "message"=>"Buku berhasil dihapus"
+      ]);
+
+      return redirect()->route('books.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix'=>'admin', 'middleware'=>['auth', 'role:admin']], function () {
   Route::resource('authors', 'AuthorController');
+  Route::resource('books', 'BookController');
 });
